173c795ae16101313ce2421fd2105e667a5d9832
Jake Hamilton <jhamilton@olin.edu>
2025-12-17 10:09:23 -0500

    Quartz sync: Dec 17, 2025, 10:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jake30123.github.io/civic-education-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
